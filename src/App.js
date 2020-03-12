3ad6f70a2392097ba4cb85bd64d2e5154c604ab9
Shafiul <shafiulbashar85@gmail.com>
2020-03-12 18:44:01 +0600

    app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
 import './App.css';
 
 import Header from './components/Header';
+
+import About from './components/pages/About';
+import Credits from './components/pages/Credits';
+// import Disclainer from './components/Pages/Disclainer';
 
 import Footer from './components/Footer';
 import LetestPhoto from './components/LetestPhoto';
@@ -10,22 +16,28 @@ import LetestPhoto from './components/LetestPhoto';
 
 function App() {
     return (
-        <div className="app">
-            <Header></Header>
-            <div className="content-bolck">
-                <div className="container">
-                    <div className="row">
-                        <div className="col">
-                            <h2>Letest photos</h2>
-                            <div className="row">
-                                <LetestPhoto></LetestPhoto>
+        <Router>
+            <div className="app">
+
+                <Header></Header>
+                <div className="content-bolck">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col">
+                                <Route exact path="/" render={props => (
+                                    <LetestPhoto></LetestPhoto>
+                                )} />
+                                <Route path="/single" component={About} />
+                                {/* <Route path="/disclainer" component={Disclainer} /> */}
+                                <Route path="/credits" component={Credits} />
+
                             </div>
                         </div>
                     </div>
                 </div>
+                <Footer></Footer>
             </div>
-            <Footer></Footer>
-        </div>
+        </Router>
     );
 
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import { Link } from 'react-router-dom'
 export default class Header extends Component {
     render() {
         return (
@@ -7,24 +7,15 @@ export default class Header extends Component {
                 <div className="header">
                     <div className="container">
                         <div className="row">
-                            <div className="col-lg-4 my-auto">
-                                <a href="/" className="logo">photoshows</a>
+                            <div className="logo col-auto my-auto">
+                                <Link to="/"><h1>Unsplash</h1></Link>
                             </div>
-                            <div className="col-lg-4 my-auto text-center">
+                            <div className="col my-auto text-center">
                                 <div className="mainmenu">
                                     <ul>
-                                        <li><a href="/">Home</a></li>
-                                        <li><a href="/">About</a></li>
-                                        <li><a href="/">Disclainer</a></li>
-                                        <li><a href="/">credits</a></li>
+                                        <li><Link to="/">Home</Link></li>
                                     </ul>
                                 </div>
-                            </div>
-                            <div className="col-lg-4 my-auto text-right">
-                                <form action="/">
-                                    <input type="text" placeholder="search keyword" />
-                                    <input type="submit" value="search" />
-                                </form>
                             </div>
                         </div>
                     </div>
